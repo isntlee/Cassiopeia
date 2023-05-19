e9cd8a2de889b1aef9883192dc18b9188c30d1a9
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-05-19 14:16:24 +0100

    Refactored navigate_ship()

--- apps/testing/views.py
+++ apps/testing/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
 from environ import Env; env = Env()
-import json, requests, time
+import json, requests, time, traceback
 
 
 def get_request(url):
@@ -11,6 +11,7 @@ def get_request(url):
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
         get_error(response)
+        return
     # print("Response : ", json.dumps(response.json(), indent=4))
     return response.json()
 
@@ -22,8 +23,8 @@ def post_request(url, payload):
     }
     response = requests.post(url, json=payload, headers=headers)
     if response.status_code != 200:
-        get_error(response)
-        return
+        error_code = get_error(response)
+        return error_code
     # print("Response : ", json.dumps(response.json(), indent=4))
     return response.json()
 
@@ -32,9 +33,9 @@ def get_error(response):
     formatted_error = json.loads(response.content.decode("utf-8"))
     error_code = formatted_error["error"]["code"]
 
-    print(response.reason, ":", response.status_code)
-    print(formatted_error) 
-    print('Error Code:', error_code)
+    # print(response.reason, ":", response.status_code)
+    # print(formatted_error) 
+    # print('Error Code:', error_code)
     return error_code
 
 
@@ -51,6 +52,11 @@ def get_yes_no_input(prompt):
 
 
 ## GET REQUESTS
+
+def start_up():
+    agent_info()
+    list_own_ships()
+    navigate_ship()
 
 
 def game_status():
@@ -80,16 +86,10 @@ def headquarters():
     try:
         url = f"https://api.spacetraders.io/v2/systems/{home_system}/waypoints/{hq_location}"
     except Exception:
-        print("Error: Location details not set, try agent info")
+        print("Error: Location details #1 not set, try agent info")
         return
     get_request(url)
 
-
-def list_own_ships():
-    global ship_symbol
-    url = "https://api.spacetraders.io/v2/my/ships"
-    info = get_request(url)
-    ship_symbol = info['data'][0]['symbol']
 
 
 def list_contracts():
@@ -109,11 +109,12 @@ def detail_contract():
     get_request(url)
 
 
+
 def list_waypoints():
     try:
         url = f"https://api.spacetraders.io/v2/systems/{home_system}/waypoints"
     except Exception:
-        print("Error: Location details not set, try agent info")
+        print("Error: Location details #2 not set, try agent info")
         return
     get_request(url)
 
@@ -122,9 +123,10 @@ def detail_waypoint():
     try:
         url = f"https://api.spacetraders.io/v2/systems/{home_system}/waypoints"
     except:
-        print("Error: Location details not set, try agent info")
+        print("Error: Location details #3 not set, try agent info")
         return
     get_request(url)
+
 
 
 def find_shipyard():
@@ -132,7 +134,7 @@ def find_shipyard():
     try:
         url = f"https://api.spacetraders.io/v2/systems/{home_system}/waypoints"
     except Exception:
-        print("Error: Location details not set, try agent info")
+        print("Error: Location details #4 not set, try agent info")
         return None
     
     info = get_request(url)
@@ -150,6 +152,14 @@ def find_shipyard():
              
     print('No shipyards found')
     return None
+
+
+def list_own_ships():
+    global ship_symbol
+    url = "https://api.spacetraders.io/v2/my/ships"
+    info = get_request(url)
+    ship_symbol = info['data'][0]['symbol']
+    return
 
 
 def list_ships():
@@ -173,7 +183,7 @@ def find_asteroids():
     try:
         url = f"https://api.spacetraders.io/v2/systems/{home_system}/waypoints"
     except Exception:
-        print("Error: Location details not set, try agent info")
+        print("Error: Location details #5 not set, try agent info")
         return None
     
     info = get_request(url)
@@ -196,18 +206,91 @@ def find_destinations():
     destinations.update({info[2]:info[0]})
     info = find_asteroids()
     destinations.update({info[2]:info[0]})
-    navigate_ship()
+    return
 
 
-def docking_destination(arrival_in, ship_symbol):
+def destination_choice():
+    quick_choice = {}
+    print("\nFrom these choices:")
+    for index, destination_type in enumerate(destinations.keys(), start=1):
+        print(f"{index}. {destination_type}")
+        quick_choice.update({index:destination_type})
+
+    num_choice = int(input("\nWhich numbered destination is correct?\n"))
+    waypoint = quick_choice.get(num_choice)
+    return waypoint
+
+
+def detailed_navigate_info(navigate_info):
+    fuel_capacity = navigate_info['data']['fuel']['capacity']
+    fuel_current = navigate_info['data']['fuel']['current']
+    fuel_index = fuel_current/fuel_capacity
+    arrival_time = datetime.fromisoformat(navigate_info['data']['nav']['route']['arrival'])
+    departure_time = datetime.fromisoformat(navigate_info['data']['nav']['route']['departureTime'])
+    arrival_in = int((arrival_time-departure_time).seconds)
+    print("\nFuel :", fuel_index)
+    print('Arriving in :', arrival_in, 'secs')
+    return (arrival_in)
+
+
+
+def docking_destination(arrival_in, ship_symbol, payload):
     time.sleep(arrival_in)
     try:
         url = f"https://api.spacetraders.io/v2/my/ships/{ship_symbol}/dock"
-        post_request(url)
+        post_request(url, payload)
+        refuel_choice(ship_symbol)
+
+    except Exception:
+        print("Error: Location details #6 not set, try agent info") 
+        traceback.print_exc()
+
+
+def dock_choice(arrival_in, ship_symbol, payload):
+    dock_choice = "\nShould we dock at our destination?\n"
+    proceed = get_yes_no_input(dock_choice)
+
+    try:
+        if proceed:
+            print("\nDocking on arrival, in c.", arrival_in, "secs\n" )
+            docking_destination(arrival_in, ship_symbol, payload)
+        else:
+            print("\nYou can chooose on arrival, in c.",arrival_in,"secs\n")
+            return
         
     except Exception:
-        print("Error: Location details not set, try agent info") 
-    
+        print ('HERE WE ARE #2')
+        return
+
+
+
+def refueling(ship_symbol):
+    try:
+        url = f"https://api.spacetraders.io/v2/my/ships/{ship_symbol}/refuel"
+        payload = {}
+        post_request(url, payload)
+
+    except Exception:
+        print("Error: Location details #7 not set, try agent info") 
+        traceback.print_exc()
+
+
+def refuel_choice(ship_symbol):
+    refuel_choice = "\nShould we refuel?\n"
+    proceed = get_yes_no_input(refuel_choice)
+
+    try:
+        if proceed:
+            print("\nRefueling now\n" )
+            refueling(ship_symbol)
+        else:
+            print("\nSuit yourself\n")
+            return 
+        
+    except Exception:
+        print ('HERE WE ARE #2')
+        traceback.print_exc()
+        return
 
 
 ## POST REQUESTS
@@ -219,48 +302,30 @@ def navigate_ship():
     except Exception:
         print("Error: Ship details not set, try list_ships")
         return
-    try:
-        destinations
-        try:
-            print("\nFrom these choices:")
-            quick_choice = {}
-            for index, destination_type in enumerate(destinations.keys(), start=1):
-                print(f"{index}. {destination_type}")
-                quick_choice.update({index:destination_type})
-
-            num_choice = int(input("\nWhich numbered destination is correct?\n"))
-            waypoint = quick_choice.get(num_choice)
-            payload = {"waypointSymbol": destinations.get(waypoint)}
-
-            info = post_request(url, payload)
-            fuel_capacity = info['data']['fuel']['capacity']
-            fuel_current = info['data']['fuel']['current']
-            fuel_index = fuel_current/fuel_capacity
-            arrival_time = datetime.fromisoformat(info['data']['nav']['route']['arrival'])
-            departure_time = datetime.fromisoformat(info['data']['nav']['route']['departureTime'])
-            arrival_in = int((arrival_time-departure_time).seconds)
-
-            print("\nFuel :", fuel_index)
-            print('Arriving in :', arrival_in, 'secs')
-
-            dock_choice = "\nShould we dock at our destination?\n"
-            proceed = get_yes_no_input(dock_choice)
-
-            if proceed:
-                print("\nDocking on arrival\n")
-                docking_destination(arrival_in, ship_symbol)
-                return
-            else:
-                print("You can chooose on arrival")
-                
-        except Exception:
-
-            ### This is still a real problem ###
-            print('HERE WE ARE #1')
-            pass
-
-    except NameError:
+    
+    if 'destinations' not in globals():
         find_destinations()
+
+    try:
+        waypoint = destination_choice()
+        payload = {"waypointSymbol": destinations.get(waypoint)}
+        navigate_info = post_request(url, payload)
+        arrival_in = detailed_navigate_info(navigate_info) 
+        dock_choice(arrival_in, ship_symbol, payload)
+
+    except Exception:
+        # print('HERE WE ARE #1')
+        # traceback.print_exc()
+        if navigate_info == 4214:
+            print("\nShip is currently in-transit to", waypoint )
+            return
+        
+        elif navigate_info == 4204:
+            print("\nShip is located at the selected destination")
+            return
+        
+        else: 
+            print('Still looking')
 
 
 def purchase_ship():
