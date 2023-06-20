d4c7fca8b71a39471cdb8902214b2cbbfda870ff
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-06-20 15:19:45 +0100

    Corrected Agent-User relationship, set to One-to-One. Dumped DB

--- apps/agents/models.py
+++ apps/agents/models.py
@@ -3,14 +3,13 @@ from gemini.users.models import User
 
 
 class Agent(models.Model):
-    symbol = models.CharField(max_length=60)
-    accountId = models.CharField(max_length=60)
-    hq = models.CharField(max_length=60)
-    faction = models.CharField(max_length=60)
+    symbol = models.CharField(max_length=60, default='symbol_default')
+    accountId = models.CharField(max_length=60, default='accountId_default')
+    hq = models.CharField(max_length=60, default='hq_default')
+    faction = models.CharField(max_length=60, default='faction_default')
     credits = models.IntegerField(null=True, blank=True)
-    agent_token = models.CharField(max_length=1000)
-    # Change this back to OneToOne when built is finished
-    user = models.ForeignKey(User, related_name='agents', on_delete=models.CASCADE)
+    agent_token = models.CharField(max_length=1000, default='token_default')
+    user = models.OneToOneField(User, related_name='agent', on_delete=models.SET_NULL, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.symbol:
